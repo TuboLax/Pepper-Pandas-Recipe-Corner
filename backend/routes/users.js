2c650a179b1522ae3bb72984bae50adc95d08d66
alexmendoza99 <alexandreamendoza99@gmail.com>
2024-03-07 11:56:48 -0800

    Changed bcrypt to argon2
    - Changed the hashing function
    - Modified App.js to be more condensed
    - Reimplemented the frontend
    Front and backend work on windows

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,45 +1,41 @@
 const express = require('express');
-const jwt=require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const argon2 = require('argon2'); // Replace bcrypt with argon2
 const UserModel = require('../models/Users.js');
 
-const userRouter= express.Router();
-const SECRET = "8aJaDbI6DtHof5jvDD75J8OfscAs0optTyF"
+const userRouter = express.Router();
+const SECRET = "8aJaDbI6DtHof5jvDD75J8OfscAs0optTyF";
 
-userRouter.post("/createAccount", async(req, res) =>
-{
-    const {username, password} = req.body; // asks for user input
+userRouter.post("/createAccount", async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = await UserModel.findOne({username}); // checks if username received from input matches any in the database
+    const user = await UserModel.findOne({ username });
 
-    if(user)
-    {
-        return res.json({ message: "User already exists."});
+    if (user) {
+        return res.json({ message: "User already exists." });
     }
-    const hashed = await bcrypt.hash(password,10); //hash password for security
-    const newUser = new UserModel({username,password: hashed}); //store new account in DB
+
+    const hashed = await argon2.hash(password); // Use argon2 to hash the password
+    const newUser = new UserModel({ username, password: hashed });
     await newUser.save();
-    res.json({message: "Account Created Successfully."});
-});
-userRouter.post("/login", async(req,res)=>
-{
-    const {username,password} = req.body;
-
-    const user = await UserModel.findOne({username});
-    if(!user)
-    {
-        return res.json({message: "User does not exist."});
-    }
-
-    const passMatches = await bcrypt.compare(password, user.password); //compares hashed passoword with hashed input
-    if(!passMatches)
-    {
-        return res.json({message: "Username / Password is Incorrect."});
-    }
-
-    const token=jwt.sign({id: user._id}, SECRET);
-    res.json({token,userID: user._id});
+    res.json({ message: "Account Created Successfully." });
 });
 
+userRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
 
-module.exports= userRouter;
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+        return res.json({ message: "User does not exist." });
+    }
+
+    const passMatches = await argon2.verify(user.password, password); // Use argon2 to verify the password
+    if (!passMatches) {
+        return res.json({ message: "Username / Password is Incorrect." });
+    }
+
+    const token = jwt.sign({ id: user._id }, SECRET);
+    res.json({ token, userID: user._id });
+});
+
+module.exports = userRouter;
---
+++ frontend/src/pages/create-recipes.js
@@ -0,0 +1,3 @@
+export const CreateRecipe = () =>{
+    return <div> Create Recipe </div>
+};
