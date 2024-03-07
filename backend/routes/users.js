23712149106bbdb1a0f55fbfa7701d743b38da68
Prabhav Goyal <prabhav.goyal@gmail.com>
2024-03-07 12:01:18 -0800

    Crypto Changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,45 +1,44 @@
 const express = require('express');
-const jwt=require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const UserModel = require('../models/Users.js');
 
 const userRouter= express.Router();
-const SECRET = "8aJaDbI6DtHof5jvDD75J8OfscAs0optTyF"
+const SECRET = "9w8ed32eHDsc923hGSDB832Hdcjsbwe7sducDH923nd";
 
-userRouter.post("/createAccount", async(req, res) =>
-{
+//Create Account
+userRouter.post("/createAccount", async(req, res) => {
     const {username, password} = req.body; // asks for user input
-
+    
     const user = await UserModel.findOne({username}); // checks if username received from input matches any in the database
-
-    if(user)
-    {
+    if (user) {
         return res.json({ message: "User already exists."});
     }
-    const hashed = await bcrypt.hash(password, 10); //hash password for security
-    const newUser = new UserModel({username, password: hashed}); //store new account in DB
+
+    const salt = crypto.randomBytes(16).toString('hex');    //salt encryption is generated
+    const hashedPassword = crypto.createHmac('sha256', salt).update(password).digest('hex');    //password is hashed with salt encryption for security
+
+    const newUser = new UserModel({ username, password: hashedPassword });  //store new account in DB
+    
     await newUser.save();
     res.json({message: "Account Created Successfully."});
 });
-userRouter.post("/login", async(req,res)=>
-{
-    const {username,password} = req.body;
+
+userRouter.post("/login", async(req,res) => {
+    const { username, password } = req.body;
 
     const user = await UserModel.findOne({username});
-    if(!user)
-    {
+    if (!user) {
         return res.json({message: "User does not exist."});
     }
 
-    const passMatches = await bcrypt.compare(password, user.password); //compares hashed passoword with hashed input
-    if(!passMatches)
-    {
-        return res.json({message: "Username / Password is Incorrect."});
+    const hashedPassword = hashPassword(password, user.password.salt);
+    if (hashedPassword !== user.password.hash) {    //compares hashed passoword with hashed input
+        return res.json({ message: "Username / Password is Incorrect." });
     }
 
-    const token=jwt.sign({id: user._id}, SECRET);
+    const token = jwt.sign({id: user._id}, SECRET);
     res.json({token,userID: user._id});
 });
-
 
 module.exports = userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const userRouter = require('./routes/users.js');
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.use("/auth", userRouter);
 
 //get for postman - Lists on webpage
 app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app!!!'});
+    res.json({mssg: 'Welcome to the app!'});
 })
 
 //database connection
@@ -25,5 +25,5 @@ mongoose.connect("mongodb+srv://saccodeproject:TermPeppa89@recipes.v3su4qy.mongo
 
 //Listening for requests
 app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
+    console.log('Listening on port', process.env.PORT);
 })
