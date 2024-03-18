20e60e162afd7dc2c6335affd47c865ee92a72a4
alinavc <alinacorpora@gmail.com>
2024-03-18 13:31:32 -0700

    Implemented Login/Register features.

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    const [cookies,setCookies] = useCookies(["accessToken"]);
+    const navigate=useNavigate();
+
+    const logout = () =>
+    {
+        setCookies("accessToken", "");
+        window.localStorage.removeItem("userID");
+        navigate("/auth");
+    }
     return (
         <div className="navbar">
             <Link to="/">Home</Link>
             <Link to="/create-recipes">Create Recipes</Link>
             <Link to="/saved-recipes">Saved Recipes</Link>
-            <Link to="/auth">Login/Register</Link>
+            {!cookies.accessToken ? (<Link to="/auth">Login/Register</Link>) : (<button onClick={logout}>Logout</button>)}
         </div>
     );
 }
--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,3 +1,133 @@
+import { useState } from "react";
+import axios from 'axios';
+import {useCookies} from 'react-cookie';
+import {useNavigate} from 'react-router-dom';
+
 export const Auth = () =>{
-    return <div> Auth </div>
+    return(
+    <div className="auth">
+        <Login />
+        <Register />
+    </div>
+    );
 };
+
+const Login = () =>
+{
+    const [username,setUsername] = useState("")
+    const[password,setPassword] = useState("")
+    const [,setCookies] = useCookies(["accessToken"])
+    const navigate = useNavigate();
+    let status="";
+    let loginBool=false;
+
+    const onSubmit = async (event) =>
+    {
+        event.preventDefault();
+
+        try{
+            const input = await axios.post("http://localhost:3000/auth/login", {username,password});
+            loginBool=input.data.logStatus;
+            console.log(loginBool);
+            if(loginBool==="true")
+            {
+                status="Login Success!";
+                document.getElementById('statusMessage').innerHTML= status;
+                setCookies("accessToken",input.data.token);
+                window.localStorage.setItem("userID", input.data.userID);
+                navigate("/");
+            }
+            else if(loginBool==="false1")
+            {
+                status=input.data.message;
+                document.getElementById('statusMessage').innerHTML= status;
+            }
+            else
+            {
+                status=input.data.message;
+                document.getElementById('statusMessage').innerHTML= status;
+            }
+        } catch(err) {
+            console.error(err);
+            }
+    }
+
+    return(<Form 
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            formType="Login"
+            onSubmit={onSubmit}
+            />
+    );
+};
+
+const Register = () =>
+{
+    const [username,setUsername] = useState("")
+    const[password,setPassword] = useState("")
+    let status="";
+    let regBool=false;
+
+    const onSubmit = async (event) =>
+    {
+        event.preventDefault(); //prevents page refresh
+        try{
+            const input = await axios.post("http://localhost:3000/auth/createAccount", {username,password});
+            regBool=input.data.regStatus;
+            console.log(regBool);
+            if(regBool===true)
+            {
+                status=input.data.message;
+                document.getElementById('statusMessage').innerHTML= status;
+            }
+            else
+            {
+                status=input.data.message;
+                document.getElementById('statusMessage').innerHTML= status;
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return(<Form 
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            formType = "Create Account"
+            onSubmit={onSubmit}
+            />
+    );
+};
+
+const Form = ({username,setUsername, password, setPassword, formType, onSubmit}) =>
+{
+    return(
+    <div className="auth-container">
+            <form onSubmit={onSubmit}>
+                <h2>{formType}</h2>
+                <div className="form-group">
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(event)=>setUsername(event.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event)=>setPassword(event.target.value)}/>
+                </div>
+                <button type="submit">Continue</button>
+                <p id='statusMessage'></p>
+            </form>
+        </div>
+    );
+}
