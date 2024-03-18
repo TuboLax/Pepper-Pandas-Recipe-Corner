3d688c1541b332b6eb637416bdcc789bc249b591
alinavc <alinacorpora@gmail.com>
2024-03-18 15:22:52 -0700

    FIXED Login/Register Components

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,13 +1,25 @@
 import './navbar.css';
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    const [cookies,setCookies] = useCookies(["accessToken"]);
+    const navigate=useNavigate();
+
+    const logout = () =>
+    {
+        setCookies("accessToken", "");
+        window.localStorage.removeItem("userID");
+        navigate("/auth");
+    }
+
     return (
         <div className="navbar">
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/create-recipes" className="nav-link">Create Recipes</Link>
             <Link to="/saved-recipes" className="nav-link">Saved Recipes</Link>
-            <Link to="/auth" className="nav-link">Login/Register</Link>
+            {!cookies.accessToken ? (<Link to="/auth">Login/Register</Link>) : (<button onClick={logout}>Logout</button>)}
         </div>
     );
 }
