0dda468d6aa66181c981b16f6d1e341db3fe7938
HenryH04 <HenryLHill04@gmail.com>
2024-03-28 23:02:30 -0700

    Search Bar Test

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,6 +3,10 @@ import bambooStirFryImage from '../assets/bamboo-stir-fry.jpg';
 import pepperPandaLogo from '../assets/pepper-panda.png';
 
 
+import { useState } from "react";
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 export const Home = () => {
     return (
         <div className="container">
@@ -25,6 +29,11 @@ export const Home = () => {
                     <img src={bambooStirFryImage} alt="Bamboo Stir Fry" />
                 </div>
             </section>
+            <body>
+        <div className="auth">
+          <SearchTitle />
+        </div>
+      </body>
             <footer>
                 <p>&copy; 2024 Pepper Panda's Recipe Corner. All rights reserved.</p>
             </footer>
@@ -32,3 +41,68 @@ export const Home = () => {
     );
 };
 
+const SearchTitle = () => {
+    const [title, setTitle] = useState("")
+    const [number, setNumber] = useState("")
+    const [, setCookies] = useCookies(["accessToken"])
+    const navigate = useNavigate();
+  
+    const onSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const test = await axios.get(`http://localhost:3000/recipes/getTitle/${title}/${number}`);
+        console.log(test.data);
+        //const input = await axios.post("http://localhost:3000/auth/login", { username, password });
+        //const loginBool = input.data.logStatus;
+        //console.log(loginBool);
+        // if (loginBool === "true") {
+        //   setCookies("accessToken", input.data.token);
+        //   window.localStorage.setItem("userID", input.data.userID);
+        //   navigate("/");
+        // } else {
+        //   alert(input.data.message);
+        // }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  
+    return (
+      <Form
+        title={title}
+        setTitle={setTitle}
+        number={number}
+        setNumber={setNumber}
+        formType="Search by Title"
+        onSubmit={onSubmit}
+      />
+    );
+  };
+
+  const Form = ({ title, setTitle, number, setNumber, formType, onSubmit }) => {
+    return (
+      <div className="auth-container">
+        <form onSubmit={onSubmit}>
+          <h2>{formType}</h2>
+          <div className="form-group">
+            <label htmlFor="title">Title: </label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="number">Number: </label>
+            <input
+              type="number"
+              id="number"
+              value={number}
+              onChange={(event) => setNumber(event.target.value)} />
+          </div>
+          <button type="submit">Continue</button>
+        </form>
+      </div>
+    );
+  }
