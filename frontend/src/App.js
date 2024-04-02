0ed8dbb00db488779f0f8bd5f9d32e57a6eca1c1
alexmendoza99 <alexandreamendoza99@gmail.com>
2024-04-02 09:51:37 -0700

    recloning issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,22 @@
-import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/home';
-import { Auth } from './pages/auth';
-import { CreateRecipe } from './pages/create-recipes';
-import { SavedRecipes } from './pages/saved-recipes';
-import { Navbar } from './components/navbar';
-import ThemeSwitcher from './components/ThemeSwitcher';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipes";
+import { SavedRecipes } from "./pages/saved-recipes";
+import { Navbar } from "./components/navbar";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(prevMode => !prevMode);
-  };
-
   return (
-    <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-      <Router>
+    <div className="App">
+      <Router> 
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/create-recipes" element={<CreateRecipe />} />
           <Route path="/saved-recipes" element={<SavedRecipes />} />
         </Routes>
-        <ThemeSwitcher isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
       </Router>
     </div>
   );
