e61188a761536a79fb74db440db54058e4ca5286
HenryH04 <HenryLHill04@gmail.com>
2024-04-02 14:20:44 -0700

    Relocation of API testing code

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -6,10 +6,6 @@ import { userGetUserID } from '../hooks/useGetUserID';
 import axios from 'axios';
 
 
-import { useState } from "react";
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
 export const Home = () => {
     const [recipes, setRecipes] = useState([]);
     const [savedRecipes, setSavedRecipes] = useState([]);
@@ -104,69 +100,3 @@ export const Home = () => {
         </div>
     );
 };
-
-const SearchTitle = () => {
-    const [title, setTitle] = useState("")
-    const [number, setNumber] = useState("")
-    const [, setCookies] = useCookies(["accessToken"])
-    const navigate = useNavigate();
-  
-    const onSubmit = async (event) => {
-      event.preventDefault();
-  
-      try {
-        const test = await axios.get(`http://localhost:3000/recipes/getTitle/${title}/${number}`);
-        console.log(test.data);
-        //const input = await axios.post("http://localhost:3000/auth/login", { username, password });
-        //const loginBool = input.data.logStatus;
-        //console.log(loginBool);
-        // if (loginBool === "true") {
-        //   setCookies("accessToken", input.data.token);
-        //   window.localStorage.setItem("userID", input.data.userID);
-        //   navigate("/");
-        // } else {
-        //   alert(input.data.message);
-        // }
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  
-    return (
-      <Form
-        title={title}
-        setTitle={setTitle}
-        number={number}
-        setNumber={setNumber}
-        formType="Search by Title"
-        onSubmit={onSubmit}
-      />
-    );
-  };
-
-  const Form = ({ title, setTitle, number, setNumber, formType, onSubmit }) => {
-    return (
-      <div className="auth-container">
-        <form onSubmit={onSubmit}>
-          <h2>{formType}</h2>
-          <div className="form-group">
-            <label htmlFor="title">Title: </label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="number">Number: </label>
-            <input
-              type="number"
-              id="number"
-              value={number}
-              onChange={(event) => setNumber(event.target.value)} />
-          </div>
-          <button type="submit">Continue</button>
-        </form>
-      </div>
-    );
-  }
