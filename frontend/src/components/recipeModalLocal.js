ed22542bfaed82c641e37f83b6a0327c2658abb3
alinavc <alinacorpora@gmail.com>
2024-04-14 16:34:31 -0700

    Recipe Modal (Popout) for both local and spoonacular calls

--- frontend/src/components/recipeModalLocal.js
+++ frontend/src/components/recipeModalLocal.js
@@ -1,7 +1,11 @@
 import './recipeModal.css';
-import { useState, useEffect } from 'react';
-import { userGetUserID } from '../hooks/useGetUserID';
-import axios from 'axios';
+import { useState} from 'react';
+import veganLogo from '../assets/food icons/vegan.png';
+import vegetarianLogo from '../assets/food icons/vegitarian.png'; //corrected my spelling
+import pescLogo from '../assets/food icons/pescatarian.PNG';
+import ketoLogo from '../assets/food icons/keto.PNG';
+import dairyFreeLogo from '../assets/food icons/dairy_free.png';
+import glutenFreeLogo from '../assets/food icons/gluten_free.png';
 
 export const RecipeModalLocal = (recipeKey) =>
 {
@@ -40,34 +44,134 @@ export const RecipeModalLocal = (recipeKey) =>
 }
 
 const Form = (recipe) =>{
-    /*
-    const[recipes,setRecipes] = useState("");
-    const [savedRecipes, setSavedRecipes] = useState("");
-    const userID = userGetUserID();
-    const recID=ID;
-    useEffect(() => {
-        const fetchSpoonacularRecipe = async() => {
-            try{
-                const response = await axios.get(`http://localhost:3000/find/id/${recID}`);
-                setRecipes(response.data);
-            } catch (err) {
-                console.log(err);
+
+    // Separate some info for HTML output
+    let hold= Object.values(recipe || {});
+    let callRecipe= (hold[0].recipeKey);
+
+    // For Cuisines -----------------------
+    let cuisineArr = (callRecipe.cuisines);
+    let holdCuisArr=[];
+    for(var i=0; i<cuisineArr.length; i++)
+    {
+        if(cuisineArr[i] !== null )
+        {
+            if(i !== cuisineArr.length-1)
+            {
+                holdCuisArr[i]=cuisineArr[i]+ ', ';
             }
-        };
-        const fetchSavedRecipe = async () => {
-            try{
-                const response = await axios.get("http://localhost:3000/recipes/savedRecipes/recipe/",{data: { recID, userID}});
-                setSavedRecipes(response.data.savedRecipes);
-            } catch (err) {
-                console.log(err);
+            else
+            {
+                holdCuisArr[i]=cuisineArr[i];
             }
-        };
-        fetchSpoonacularRecipe();
-        fetchSavedRecipe();
-    }, [recID]);
-*/
-    console.log(recipe);
-    //console.log(savedRecipes);
-    //console.log(ID);
-    return(0);
+        }
+    }
+    if(holdCuisArr.length===0)
+    {
+        holdCuisArr[0]='No Tags Found'
+    }
+    // -------------------------------------
+    //For Diets
+    var veganBool,
+    vegtrnBool,
+    pescBool,
+    ketoBool,
+    dfBool,
+    gfBool = false;
+    let dietArr= callRecipe.diets;
+    for(var i=0; i<dietArr.length; i++)
+    {
+        if(dietArr[i] === 'Vegan')
+        {
+            veganBool=true;
+        }
+        else if(dietArr[i] === 'Vegetarian')
+        {
+            vegtrnBool=true;
+        }
+        else if(dietArr[i] === 'Pescatarian')
+        {
+            pescBool=true;
+        }
+        else if(dietArr[i] === 'Ketogenic')
+        {
+            ketoBool= true;
+        }
+        else if(dietArr[i] === 'Dairy Free')
+        {
+            dfBool=true;
+        }
+        else if(dietArr[i] === 'Gluten Free')
+        {
+            gfBool=true;
+        }
+    }
+// --------------------------------------------
+    console.log(callRecipe);
+    console.log(callRecipe.sourceUrl)
+
+    return(
+        <div className='popoutBody'>
+            <h1 className='recName'>{callRecipe.title}</h1>
+            <h3 className='recCategories'>Recipe Categories</h3>
+            <>
+                {veganBool && (
+                    <img src={veganLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id="vegan" alt="Vegan" title="Vegan"></img>
+                )}
+                {vegtrnBool && (
+                    <img src={vegetarianLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id="vegetarian" alt='Vegetarian' title="Vegetarian"></img>
+                )}
+                {pescBool && (
+                    <img src={pescLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id="pescatarian" alt='Pescatarian' title="Pescatarian"></img>
+                )}
+                {ketoBool && (
+                    <img src={ketoLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id= "keto" alt='Ketogenic' title="Ketogenic"></img>
+                )}
+                {gfBool && (
+                    <img src={glutenFreeLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id="glutenFree" alt='Gluten Free' title="Gluten Free"></img>
+                )}
+                {dfBool && (
+                <img src={dairyFreeLogo} style={{width:'80px', height:'80px', borderRadius: '50%'}} className='logos' id= "dairyFree" alt='Dairy Free' title="Dairy Free"></img>
+                )}
+            </>
+            <>
+                <p></p>
+                <span style={{padding:'5px'}}> <b>Servings:</b> {callRecipe.servings}</span>
+                <span style={{padding:'5px'}}><b>Preparation Time:</b> {callRecipe.readyInMinutes} Minutes</span>
+            </>
+            <>
+            <div className='cuisTagsBox'>
+                <p></p>
+                <span><b>Cuisine Tags:</b> </span>
+                <span>{holdCuisArr}</span>
+            </div>
+            </>
+            <div className='boxes' id='localBoxes'>
+                        <div className='ingBox'>
+                            <h3 className='ingHead'><b>Ingredients:</b></h3>
+                            <div>
+                                {callRecipe.extendedIngredients.map((ing, index) => {
+                                    return ( // needed a return for the mapping
+                                        <div key={index}>
+                                            <input className='ingCheck' type="checkbox" name={ing}/>
+                                            <label className='ingLabel'> {ing}</label>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className='dirBox'>
+                            <h3 className='directionsHeader'><b>Directions:</b></h3>
+                            <ol>
+                                <div className='directionsList'>
+                                    {callRecipe.instructions.map((step, index) => (
+                                        <li key={index}>{step}</li>
+                                    ))}
+                                </div>
+                            </ol>
+                        </div>
+                </div>
+            <a className="sourceURL" href={callRecipe.sourceURL} target='_blank'>Visit Original Recipe</a>
+        </div>
+    );
 }
