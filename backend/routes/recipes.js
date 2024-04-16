043700b1912eb8b6f43d1f9467b46740b646bbc0
Prabhav Goyal <prabhav.goyal@gmail.com>
2024-04-16 15:59:38 -0700

    Newly created recipes are automatically saved
    Whenever a user creates a recipe. It is automatically saved in your saved recipes page.

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -24,6 +24,17 @@ recipesRouter.post("/", async (req, res) => {
     try {
         const recipe = new RecipeModel(req.body);           //RecipeModel to create new recipe with all properties from body
         const response = await recipe.save();
+
+        const userID = recipe.userOwner;
+        const user = await UserModel.findById(userID);
+        if (!user) {
+            return res.status(404).json({
+                message: "User Not Found."
+            })
+        }
+
+        user.savedRecipes.push(recipe);
+        await user.save();
         res.status(201).json(response);
     } catch (err) {
         res.status(500).json({
@@ -75,7 +86,7 @@ recipesRouter.get("/savedRecipes/:userID", async (req, res) => {
         });
     } 
 });
-//get all saved recipes by IDs in user's savedRecipe array by userID (for frontend view on saved recipes page) 
+//get all saved recipes by IDs in user's savedRecipe array by userID  
 recipesRouter.get("/savedRecipes/ids/:userID", async (req, res) => {
     try {
         const userID = req.params.userID;                        
@@ -96,7 +107,7 @@ recipesRouter.get("/savedRecipes/ids/:userID", async (req, res) => {
     } 
 });
 
-//get all saved recipes by IDs in user's savedRecipe array by userID (for frontend view on saved recipes page) 
+//get all saved recipes by IDs in user's savedRecipe array by userID  
 recipesRouter.post("/savedRecipes/recipe/:recipeID", async (req, res) => {
     try {
         const recipeID = req.params.recipeID;                        
@@ -197,62 +208,3 @@ recipesRouter.patch("/updatedRecipes", async (req, res) => {
 });
 
 module.exports = recipesRouter;
-
-/*
-//finds and updates recipe by ID locally by userID
-recipesRouter.patch("/:recipeID", async (req, res) => {
-    try {
-        const recipeID = req.params.recipeID;                       //get recipeID
-        const { updatedData } = req.body;                           //get new data and properties
-        const recipe = await RecipeModel.findById(recipeID);        //find recipe by ID
-        if (!recipe) {                                              //validate if recipe was found
-            return res.status(404).json({ 
-                message: "Recipe Not Found." 
-            });
-        }
-
-        const userID = req.user.id;                        //get userID - authenticate users and attach their IDs to request object
-        if (recipe.userOwner.toString() !== userID) {       //validates if authenticated user is owner of recipe
-            return res.status(403).json({
-                message: "You are not Authorized to Delete this Recipe."
-            });
-        }
-
-        const updateRecipe = await RecipeModel.findByIdAndUpdate(           //update recipe with specific fields of new data by ID
-            recipeID, { $set: updatedData }, { new: true }
-        );       
-        res.json({ 
-            message: "Recipe Updated Successfully.",
-            recipe: updateRecipe 
-        });
-    } catch (err) {
-        res.status(500).json({
-            message: "Internal Server Error"
-        });
-    }
-});
-*/
-
-/**
- * // Middleware to verify JWT token
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ 
-            message: "Unauthorized. Token not found." 
-        });
-    }
-
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        if (err) {
-            console.error("Error verifying token:", err);
-            return res.status(401).json({ 
-                message: "Unauthorized. Invalid token." 
-            });
-        }
-        req.user = decoded; // Attach decoded user object to request
-        next();
-    });
-};
- */
